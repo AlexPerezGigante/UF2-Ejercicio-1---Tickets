e859f7f00a8e12914307a790d5c07b3233bef0d7
perezgigantealex <perezgigantealex@fpllefia.com>
2023-11-22 19:52:26 +0100

    Creamos bd.js con 3 ejemplos.

---
+++ bd/bd.js
@@ -0,0 +1,17 @@
+export const comentarioBD = [
+  {
+    autor: 'Javier Caraculo',
+    fecha: '12/10/2023',
+    comentario: 'Comentario 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis delectus saepe, animi ratione voluptates doloribus sint harum amet minus eius perspiciatis! Ea minima quos alias at laboriosam dolorum numquam!'
+  },
+  {
+    autor: 'Javier Caraculo',
+    fecha: '13/10/2023',
+    comentario: 'Comentario 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis delectus saepe, animi ratione voluptates doloribus sint harum amet minus eius perspiciatis! Ea minima quos alias at laboriosam dolorum numquam!'
+  },
+  {
+    autor: 'Javier Caraculo',
+    fecha: '14/10/2023',
+    comentario: 'Comentario 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis delectus saepe, animi ratione voluptates doloribus sint harum amet minus eius perspiciatis! Ea minima quos alias at laboriosam dolorum numquam!'
+  }
+]
